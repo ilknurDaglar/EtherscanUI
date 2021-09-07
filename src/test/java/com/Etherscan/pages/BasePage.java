cc63ef769bd8808a1bc49e3af304f68b5492f5c3
ilknur <ilknur_daglar@yahoo.com>
2021-09-08 00:07:36 +0200

    initials comment

---
+++ src/test/java/com/Etherscan/pages/BasePage.java
@@ -0,0 +1,12 @@
+package com.Etherscan.pages;
+
+import com.Etherscan.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public abstract class BasePage {
+
+    public BasePage() {
+
+        PageFactory.initElements(Driver.get(), this);
+    }
+}
